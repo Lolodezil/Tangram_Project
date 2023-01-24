fb850c618bad2fb85bb5a49a7577cb31e58808a6
Lolodezil <henon.logan@hotmail.com>
2023-01-24 12:10:37 +0100

    Update+

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 let tangram = document.querySelector(".tangram");
 let button_carre = document.querySelector('.button--carre');
 let button_tempo = document.querySelector('.button--tempo');
+let button_autre = document.querySelector('.button--autre');
+
 
 button_carre.addEventListener('click', ()=>{
   tangram.classList.replace("tangram--tempo","tangram--carre");
@@ -8,4 +10,8 @@ button_carre.addEventListener('click', ()=>{
 
 button_tempo.addEventListener('click', ()=>{
     tangram.classList.replace("tangram--carre","tangram--tempo");
+})
+
+button_autre.addEventListener('click', ()=>{
+    tangram.classList.replace("tangram--carre","tangram--autre");
 })
